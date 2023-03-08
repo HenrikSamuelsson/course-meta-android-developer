cbc3b821d348519e407cbdc342b90becbc124717
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-03-08 07:03:20 +0100

    Add part of code for the bank account project.

--- programming-fundamentals-in-kotlin/week-04/bank-account-project/bank-account.kt
+++ programming-fundamentals-in-kotlin/week-04/bank-account-project/bank-account.kt
@@ -25,5 +25,45 @@ fun main() {
     }
   
     //Step 4
-    println("You have created a ${accountType} account.")        
+    println("You have created a ${accountType} account.")
+
+    var accountBalance = (0..1000).random() 
+
+    println("The current balance is ${accountBalance} dollars.") 
+
+    val money = (0..1000).random() 
+    println("The amount you transferred is ${money} dollars.") 
+
+    // Step 2 
+    var output = 0 
+     
+    // Step 4 
+    output = withdraw(money) 
+    println("The amount you withdrew is ${output} dollars.") 
+
+    // Step 6 
+    output = debitWithdraw(money) 
+    println("The amount you withdrew is ${output} dollars.") 
+
+    // Step 7 
+    fun deposit(amount: Int): Int { 
+        accountBalance += amount 
+        println("You successfully deposited ${amount} dollars. The current balance is ${accountBalance} dollars.") 
+        return amount 
+    } 
+ 
+
+    // Step 8 
+
+    output = deposit(money) 
+    println("The amount you deposited is ${output} dollars.") 
+
+ 
+    // Step 10 
+    output = creditDeposit(money) 
+    println("The amount you deposited is ${output} dollars.") 
+
+ 
+
+ 
 }
