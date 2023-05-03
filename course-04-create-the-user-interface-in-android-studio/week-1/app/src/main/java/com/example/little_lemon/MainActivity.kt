4a4b4819770776dc2801358a2da88694b2f20201
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-05-03 07:08:40 +0200

    Code for week 1 exerecise.

--- course-04-create-the-user-interface-in-android-studio/week-1/app/src/main/java/com/example/little_lemon/MainActivity.kt
+++ course-04-create-the-user-interface-in-android-studio/week-1/app/src/main/java/com/example/little_lemon/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.little_lemon.ui.theme.LittleLemonTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +24,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    RestaurantName(
+                        name = stringResource(id = R.string.title),
+                        size = 32)
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun RestaurantName(name: String, size: Int) {
+    Text(
+        text = name,
+        fontSize = size.sp
+    )
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    LittleLemonTheme {
-        Greeting("Android")
-    }
+fun RestaurantNamePreview() {
+    RestaurantName(
+        name = stringResource(id = R.string.title),
+        size = 32
+    )
 }
